af7324ad5f88d7d7c00835b168ebe32d080e261b
s22051-pj-lukasz-kos <s22051@pjwstk.edu.pl>
2021-06-13 00:13:45 +0200

    Adding homework to lab 3

--- s22051-pj-lukasz-kos/zajecia3/zadanie_domowe.sh
+++ s22051-pj-lukasz-kos/zajecia3/zadanie_domowe.sh
@@ -1,2 +1,16 @@
 #!/bin/bash
 
+# polskie imiona
+echo 'Polskie imiona'
+grep -e "^[[:upper:]][[:lower:]]\{1,\}[[:space:]]" "tekst.txt"
+echo
+
+# kod pocztowy
+echo 'Kod pocztowy' 
+grep -e "^[[:digit:]]\{2\}-[[:digit:]]\{3\}[[:space:]]" "tekst.txt"
+echo
+
+# email
+echo 'email'
+grep -e "^[[:lower:]][[:lower:][:digit:]._+-]\{1,\}@[[:lower:]]\{1,\}\.[[:lower:]]\{1,\}[[:space:]]" "tekst.txt"
+
